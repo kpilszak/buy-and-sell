eacae5ce71de9b5a82bf7380db150b1a55c94d4c
kpilszak <klaudia.pilszak@gmail.com>
2023-07-12 06:38:40 -0400

    Create listing-details-page component

--- buy-and-sell-frontend/src/app/app.module.ts
+++ buy-and-sell-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListingsPageComponent
+    ListingsPageComponent,
+    ListingDetailPageComponent
   ],
   imports: [
     BrowserModule,
