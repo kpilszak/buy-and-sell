6c2225f2b715ad1ca10219d388a59559e46af996
kpilszak <klaudia.pilszak@gmail.com>
2023-07-26 06:44:43 -0400

    Add routing to details of listing

--- buy-and-sell-frontend/src/app/app-routing.module.ts
+++ buy-and-sell-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 
 const routes: Routes = [
   { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' }
+  { path: 'listings/:id', component: ListingDetailPageComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
