b6b14805b96cfaa7cf62daa65bc41d3a2e2194de
kpilszak <klaudia.pilszak@gmail.com>
2023-07-27 06:45:36 -0400

    Add routing to contact page

--- buy-and-sell-frontend/src/app/app-routing.module.ts
+++ buy-and-sell-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 const routes: Routes = [
   { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' }
   { path: 'listings/:id', component: ListingDetailPageComponent, pathMatch: 'full' }
+  { path: 'contact/:id', component: ContactPageComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
