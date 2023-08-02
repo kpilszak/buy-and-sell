06141cf29d63f14cea1d0feb7bc2e6992ddeee34
kpilszak <klaudia.pilszak@gmail.com>
2023-08-02 06:47:17 -0400

    Add routing to my listings

--- buy-and-sell-frontend/src/app/app-routing.module.ts
+++ buy-and-sell-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 
 const routes: Routes = [
   { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' }
   { path: 'listings/:id', component: ListingDetailPageComponent, pathMatch: 'full' }
   { path: 'contact/:id', component: ContactPageComponent, pathMatch: 'full' }
   { path: 'edit-listing/:id', component: EditListingPageComponent, pathMatch: 'full' }
+  { path: 'my-listings', component: MyListingsPageComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
