5b5d20d7c20f0b547ad09e6c5938e84ebdbeb39a
kpilszak <klaudia.pilszak@gmail.com>
2023-08-18 17:02:17 -0400

    Assing listing with id from path in listing detail component

--- buy-and-sell-frontend/src/app/listing-detail-page/listing-detail-page.component.ts
+++ buy-and-sell-frontend/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -8,12 +10,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListingDetailPageComponent implements OnInit {
 
+  listing?: Listing;
+
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
   }
 
 }
