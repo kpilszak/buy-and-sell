8c78c974ecc542b6d5950b0b1eadc40977c82c77
kpilszak <klaudia.pilszak@gmail.com>
2023-08-30 15:37:16 -0400

    Create my listings component structure

--- buy-and-sell-frontend/src/app/my-listings-page/my-listings-page.component.ts
+++ buy-and-sell-frontend/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeMyListings } from '../fake-data';
 
 @Component({
   selector: 'app-my-listings-page',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyListingsPageComponent implements OnInit {
 
+  listings: Listing[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.listings = fakeMyListings;
   }
 
 }
