b9b9600b068528c3042ef11b60a1f4018347999e
kpilszak <klaudia.pilszak@gmail.com>
2023-09-08 15:55:37 -0400

    Create listing-data-form component

---
+++ buy-and-sell-frontend/src/app/listing-data-form/listing-data-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-data-form',
+  templateUrl: './listing-data-form.component.html',
+  styleUrls: ['./listing-data-form.component.css']
+})
+export class ListingDataFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
