be5b635257496eeb970501bbf792b80941fceb85
kpilszak <klaudia.pilszak@gmail.com>
2023-09-12 15:59:21 -0400

    Create listing data form component structure

--- buy-and-sell-frontend/src/app/listing-data-form/listing-data-form.component.ts
+++ buy-and-sell-frontend/src/app/listing-data-form/listing-data-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listing-data-form',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingDataFormComponent implements OnInit {
 
-  constructor() { }
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
