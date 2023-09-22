83c9f993ebe312b5ac78d8d509e6b9b1d47472a6
kpilszak <klaudia.pilszak@gmail.com>
2023-09-22 16:35:20 -0400

    Change new listing page component to use listing data form component

--- buy-and-sell-frontend/src/app/new-listing-page/new-listing-page.component.ts
+++ buy-and-sell-frontend/src/app/new-listing-page/new-listing-page.component.ts
@@ -8,10 +8,6 @@ import { Router } from '@angular/router';
 })
 export class NewListingPageComponent implements OnInit {
 
-  name: string = '';
-  description: string = '';
-  price: string = '';
-
   constructor(
     private router: Router,
   ) { }
