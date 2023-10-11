12cd2717e9844fe1043d3cc654953557090213f0
kpilszak <klaudia.pilszak@gmail.com>
2023-10-11 15:41:06 -0400

    Create simple hapi server

---
+++ buy-and-sell-backend/src/server.js
@@ -0,0 +1,11 @@
+import Hapi from '@hapi/hapi';
+
+const start = async () => {
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost',
+    });
+
+    await server.start();
+    console.log(`Server is listening on ${server.info.uri}`);
+}
