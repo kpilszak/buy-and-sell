3d694ba31506f69f2fd2dea3dffcbc04ff7896f5
kpilszak <klaudia.pilszak@gmail.com>
2023-10-12 15:42:12 -0400

    Add error handling method

--- buy-and-sell-backend/src/server.js
+++ buy-and-sell-backend/src/server.js
@@ -9,3 +9,10 @@ const start = async () => {
     await server.start();
     console.log(`Server is listening on ${server.info.uri}`);
 }
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    process.exit(1);
+})
+
+start();
