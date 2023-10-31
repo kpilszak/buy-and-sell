0701f7714ce23c50f65d64b4a9f3d59ee04ef03d
kpilszak <klaudia.pilszak@gmail.com>
2023-10-31 12:09:53 -0400

    Convert mysql query function into a promise

--- buy-and-sell-backend/src/database.js
+++ buy-and-sell-backend/src/database.js
@@ -6,3 +6,14 @@ const connection = mysql.createConnection({
     password: 'abc123!',
     database: 'buy-and-sell',
 });
+
+export const db = {
+    connect: () => connection.connect(),
+    query: (queryString, escapedValues) => 
+        new Promise((resolve, reject) => {
+            connection.query(queryString, escapedValues, (error, results, fields) => {
+                if (error) reject(error);
+                resolve({ results, fields });
+            })
+        })
+}
