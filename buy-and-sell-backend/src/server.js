2d49de28844a707e9f31a717d54c97e8e3004931
kpilszak <klaudia.pilszak@gmail.com>
2023-11-03 15:26:16 -0400

    When server stops, disconnect from db

--- buy-and-sell-backend/src/server.js
+++ buy-and-sell-backend/src/server.js
@@ -2,8 +2,10 @@ import Hapi from '@hapi/hapi';
 import routes from './routes';
 import db from './database';
 
+let server;
+
 const start = async () => {
-    const server = Hapi.server({
+    server = Hapi.server({
         port: 8000,
         host: 'localhost',
     });
@@ -19,6 +21,15 @@ const start = async () => {
 process.on('unhandledRejection', err => {
     console.log(err);
     process.exit(1);
-})
+});
+
+process.on('SIGINT', async () => {
+    console.log('Stopping server...');
+
+    await server.stop({ timeout: 10000 });
+    db.end();
+    console.log('Server stopped');
+    process.exit(0);
+});
 
 start();
