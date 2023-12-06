eeb7cfe21a67d7f7ae6f3a5061ce6d82fe3d45b4
kpilszak <klaudia.pilszak@gmail.com>
2023-12-06 06:40:49 -0400

    Use listings service in listing detail page component

--- buy-and-sell-frontend/src/app/listing-detail-page/listing-detail-page.component.ts
+++ buy-and-sell-frontend/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Listing } from '../types';
-import { fakeListings } from '../fake-data';
+import { ListingsService } from '../listings.service';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -10,15 +10,21 @@ import { fakeListings } from '../fake-data';
 })
 export class ListingDetailPageComponent implements OnInit {
 
+  isLoading: boolean = true;
   listing?: Listing;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private listingsService: ListingsService
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find(listing => listing.id === id);
+    this.listingsService.getListingById(id).subscribe(listing => {
+      this.listing = listing;
+      this.isLoading = false;
+    })
+    this.listingsService.addViewToListing(id).subscribe(() => console.log('Views updated!'));
   }
 
 }
