85b9655c6fb6ed084ffcda7f906d7a80b8256282
kpilszak <klaudia.pilszak@gmail.com>
2023-12-29 06:41:51 -0400

    Add authentication to endpoints

--- buy-and-sell-frontend/src/app/listings.service.ts
+++ buy-and-sell-frontend/src/app/listings.service.ts
@@ -61,23 +61,42 @@ export class ListingsService {
   }
 
   deleteListing(id: string): Observable<any> {
-    return this.http.delete(`/api/listings/${id}`);
+    return new Observable<any>(observer => {
+      this.auth.user.subscribe(user => {
+        user && user.getIdToken().then(token => {
+          this.http.delete(`/api/listings/${id}`, httpOptionsWithAuthToken(token))
+          .subscribe(() => observer.next());
+        })
+      })
+    })
   }
 
   createListing(name: string, description: string, price: number): Observable<Listing> {
-    return this.http.post<Listing>(
-      '/api/listings',
-      { name, description, price },
-      httpOptions
-    );
+    return new Observable<Listing>(observer => {
+      this.auth.user.subscribe(user => {
+        user && user.getIdToken().then(token => {
+          this.http.post<Listing>(
+            '/api/listings',
+            { name, description, price },
+            httpOptionsWithAuthToken(token),
+          ).subscribe(() => observer.next());
+        })
+      })
+    })
   }
 
   editListing(id: string, name: string, description: string, price: number): Observable<Listing> {
-    return this.http.post<Listing>(
-      `/api/listings/${id}`,
-      { name, description, price },
-      httpOptions
-    );
+    return new Observable<Listing>(observer => {
+      this.auth.user.subscribe(user => {
+        user && user.getIdToken().then(token => {
+          this.http.post<Listing>(
+            `/api/listings/${id}`,
+            { name, description, price },
+            httpOptionsWithAuthToken(token)
+          ).subscribe(() => observer.next());
+        })
+      })
+    })
   }
 
 }
